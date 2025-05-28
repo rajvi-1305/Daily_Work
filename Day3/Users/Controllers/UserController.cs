e96cf89fa330711762e7d2852dd9c9ebb22e93a0
Rajvi <rajvimangroliya1305@gmail.com>
2025-05-29 00:38:45 +0530

    Day3 Work

---
+++ Day3/Users/Controllers/UserController.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Users.Models;
+using Users.Services;
+
+namespace Users.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController: ControllerBase
+    {
+        private readonly UserService _userService;
+
+        public UserController(UserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet]
+        public ActionResult<List<User>> GetAllUsers()
+        {
+            List<User> users = _userService.GetUsers();
+            if(users == null || users.Count == 0)
+            {
+                return NotFound("User Not Found");
+            }
+            else
+            {
+                return Ok(users);
+            }
+        }
+
+        [HttpGet("id")]
+        public ActionResult<List<User>> GetUser(int id) 
+        { 
+            User user = _userService.GetUserById(id);
+            if (user == null)
+            {
+                return NotFound("User Not Found");
+            }
+            else {
+                return Ok(user);
+            }
+        }
+
+
+        [HttpPost]
+        public ActionResult AddUser(User book)
+        {
+            _userService.AddUser(book);
+            return Ok("User Cretaed successfully");
+        }
+
+        [HttpPut]
+        public ActionResult UpdateUserDetails(User userUpdate)
+        {
+            int userStatus = _userService.UpdateUser(userUpdate);
+            if (userStatus == -1)
+            {
+                return NotFound("User Not Found");
+            }
+            else if (userStatus == 1)
+            {
+                return Ok("User Details updated successfully");
+            }
+            else
+            {
+                return BadRequest("Bad request");
+            }
+        }
+
+        [HttpDelete]
+        public ActionResult DeleteUserDetails(int id)
+        {
+            int deleteStatus = _userService.DeleteUser(id);
+            if (deleteStatus == -1)
+            {
+                return NotFound("User Not found");
+            }
+            else if (deleteStatus == 1)
+            {
+                return Ok("User Details Deleted Successfully");
+            }
+            else
+            {
+                return BadRequest("Bad request");
+            }
+        }
+
+    }
+
+}
---
+++ Day3/Users/Services/UserService.cs
@@ -0,0 +1,90 @@
+﻿using Users.Models;
+using static System.Reflection.Metadata.BlobBuilder;
+
+namespace Users.Services
+{
+    public class UserService
+    {
+        private List<User> _users;
+        public UserService() 
+        {
+            _users = new List<User>();
+            _users.Add(new User()
+            {
+                Id = 1,
+                FirstName = "Rajvi",
+                LastName = "Mangroliya",
+                Email = "rm@gmail.com",
+                PhoneNumber = "1234567890"
+            });
+            _users.Add(new User()
+            {
+                Id = 2,
+                FirstName = "Alice",
+                LastName = "Doe",
+                Email = "ad@gmail.com",
+                PhoneNumber = "1111111111"
+            });
+            _users.Add(new User()
+            {
+                Id = 3,
+                FirstName = "John",
+                LastName = "Smith",
+                Email = "js@gmail.com",
+                PhoneNumber = "2345678901"
+            });
+        }
+
+        public List<User> GetUsers() 
+        {
+            return _users;
+        }
+
+        public User GetUserById(int id) 
+        {
+            User user = _users.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+            {
+                return null;
+            }
+            return user;
+        }
+
+        public void AddUser(User user) 
+        {
+            user.Id = _users.Count + 1;
+            _users.Add(user);
+        }
+
+        public int UpdateUser(User user) 
+        {
+            User userUpdate = GetUserById(user.Id);
+            if (userUpdate == null)
+            {
+                return -1;
+            }
+            else
+            {
+                userUpdate.FirstName = user.FirstName;
+                userUpdate.LastName = user.LastName;
+                userUpdate.Email = user.Email;
+                userUpdate.PhoneNumber = user.PhoneNumber;
+                return 1;
+            }
+        }
+
+        public int DeleteUser(int id) 
+        {
+            User userDelete = GetUserById(id);
+            if (userDelete == null)
+            {
+                return -1;
+            }
+            else
+            {
+                _users.Remove(userDelete);
+                return 1;
+            }
+        }
+    }
+}
