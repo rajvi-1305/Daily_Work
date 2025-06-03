49e8f58171fbe8af5a6da3dfc2aefb525327fbbe
Rajvi <rajvimangroliya1305@gmail.com>
2025-06-03 15:46:01 +0530

    Day6 commit

---
+++ Day6/Users.DataAccess/Repositories/MissionSkillRepository.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Users.DataAccess.Models;
+
+namespace Users.DataAccess.Repositories
+{
+    public class MissionSkillRepository
+    {
+        private readonly UserDbContext _missionDbContext;
+        public MissionSkillRepository(UserDbContext missionDbContext)
+        {
+            _missionDbContext = missionDbContext;
+        }
+
+        public List<MissionSkiil> GetMissionSkills()
+        {
+            return _missionDbContext.MissionSkiils.ToList();
+        }
+
+        public MissionSkiil GetMissionSkillById(int id)
+        {
+            MissionSkiil user = _missionDbContext.MissionSkiils.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+            {
+                return null;
+            }
+            return user;
+        }
+
+        public List<MissionSkiil> GetSkill(string skill)
+        {
+            List<MissionSkiil> users = _missionDbContext.MissionSkiils.Where(u => u.SkillName.Equals(skill)).ToList();
+            return users;
+        }
+
+        public void AddMissionSkill(MissionSkiil mSkill)
+        {
+            _missionDbContext.MissionSkiils.Add(mSkill);
+            _missionDbContext.SaveChanges();
+        }
+
+        public int UpdateMissionSkill(MissionSkiil mSkill)
+        {
+            MissionSkiil mSkillUpdate = GetMissionSkillById(mSkill.Id);
+            if (mSkillUpdate == null)
+            {
+                return -1;
+            }
+            else
+            {
+                mSkillUpdate.SkillName = mSkill.SkillName;
+                mSkillUpdate.Status = mSkill.Status;
+                _missionDbContext.SaveChanges();
+                return 1;
+            }
+        }
+
+        public int DeleteMissionSkill(int id)
+        {
+            MissionSkiil mSkillDelete = GetMissionSkillById(id);
+            if (mSkillDelete == null)
+            {
+                return -1;
+            }
+            else
+            {
+                _missionDbContext.MissionSkiils.Remove(mSkillDelete);
+                _missionDbContext.SaveChanges();
+                return 1;
+            }
+        }
+    }
+}
---
+++ Day6/Users.DataAccess/UserDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Users.DataAccess.Models;
+
+namespace Users.DataAccess
+{
+    public class UserDbContext: DbContext
+    {
+        public UserDbContext(DbContextOptions<UserDbContext> options): base(options) 
+        {
+
+        }
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<MissionSkiil> MissionSkiils { get; set; }
+    }
+}
